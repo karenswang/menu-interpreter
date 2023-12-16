531b4233c1b3bc3ccf5066f9562101b87c7013ee
Zoe Chen <71539423+zoechenry@users.noreply.github.com>
2023-12-16 08:39:23 -0500

    Update registerUser.py

--- lambda/registerUser.py
+++ lambda/registerUser.py
@@ -1,9 +1,12 @@
 import json
 import boto3
+import bcrypt
+import jwt
+import time
 
-# Initialize a DynamoDB client
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('user')
+SECRET_KEY = "vBufvEPGHrZ0ii_y0Fw4NsFY2x9IKocy"
 
 def lambda_handler(event, context):
     # Extract username and password from the event
@@ -20,12 +23,14 @@ def lambda_handler(event, context):
             'statusCode': 400,
             'body': json.dumps('User already exists.')
         }
+    # Hash the password
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
     # Add new user without preferences
     table.put_item(Item={
         'username': username,
-        'password': password,
-        'isLoggedIn': False,
+        'password': hashed_password,
+        #'isLoggedIn': False,
         # Initialize preferences as an empty object
         'preferences': {}
     })
